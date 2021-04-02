3ed089b2a02f3dd774071dc05c741e59646fc10d
hbuiOnline <61067968+hbuiOnline@users.noreply.github.com>
2021-04-01 22:53:22 -0500

    password_reset_sent

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,13 @@ urlpatterns = [
     # Password Recovery system
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="app/passwordRecovery/password_reset.html"),
          name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="app/passwordRecovery/password_reset_sent.html"),
          name="password_reset_done"),
+
     path('reset/<uidb64>/<token>//', auth_views.PasswordResetConfirmView.as_view(),
          name="password_reset_confirm"),
+
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
          name="password_reset_complete"),
 
